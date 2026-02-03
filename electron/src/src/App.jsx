438d6a5baa79e9f11d354b1fe4452f85c8a497d9
keremcihaan <keremcihaan@gmail.com>
2026-02-04 01:48:01 +0300

    Create App.jsx

---
+++ electron/src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Teknik Servis Uygulaması Çalışıyor ✅</h1>
+      <p>Birazdan tüm özellikleri ekleyeceğiz.</p>
+    </div>
+  )
+}
